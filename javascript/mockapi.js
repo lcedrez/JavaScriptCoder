62215353eb250c2473422d5137233ecdb97f251a
lcedrez <102386169+lcedrez@users.noreply.github.com>
2022-07-21 13:40:25 -0300

    imagenes

--- javascript/mockapi.js
+++ javascript/mockapi.js
@@ -1,3 +1,14 @@
 
 
 
+{"cod_articulo":"22427","nombre":"The Beatles 1964 Pinball Machine - Gold Edition","descripcion":"Please note: this item is only available to ship within the US. Select White Glove Shipping at checkout and get set-up included with delivery!The Beatles 1964 Pinball Machine - Gold EditionDimensionsH: 75.5 D: 55 W: 27 250 LBS","precio":4500.00,"descuento":0,"categoria":"Toy","color":"Multi Color","imagen":"Imagenes/E-Commerce/pinball.jpg","cantidad":1,"subTotal":4500.00,"size":0,"subCategoria":"Outlet","precDescuento":0},
+
+{"cod_articulo":"22428","nombre":"The Beatles X Section 119 Abbey Road Light Weight Zip Up Hoodie","descripcion":"PLEASE NOTE: This item will ship in approximately 4-6 weeks.","precio":70.00,"descuento":0,"categoria":"Outfit","color":"Black","imagen":"Imagenes/E-Commerce/out1.jpg","cantidad":1,"subTotal":70.00,"size":0,"subCategoria":"Outlet","precDescuento":0}
+
+{"cod_articulo":"22429","nombre":"Let It Be Apple Cream Onesie","descripcion":"Let It Be Apple Cream Onesie","precio":30.00,"descuento":10,"categoria":"Outfit","color":"White - Green","imagen":"Imagenes/E-Commerce/out2.jpg","cantidad":1,"subTotal":30.00,"size":0,"subCategoria":"Outlet","precDescuento":0}
+
+{"cod_articulo":"22430","nombre":"Help! Yellow Crewneck Help! Yellow Crewneck","descripcion":"Show your love for The Beatles with this crewneck sweatshirt!","precio":60.00,"descuento":20,"categoria":"Outfit","color":"Yellow","imagen":"Imagenes/E-Commerce/out3.jpg","cantidad":1,"subTotal":60.00,"size":0,"subCategoria":"Outlet","precDescuento":0}
+
+{"cod_articulo":"22429","nombre":"Let It Be Apple Cream Onesie","descripcion":"Let It Be Apple Cream Onesie","precio":30.00,"descuento":10,"categoria":"Outfit","color":"White - Green","imagen":"Imagenes/E-Commerce/out2.jpg","cantidad":1,"subTotal":30.00,"size":0,"subCategoria":"Outlet","precDescuento":0}
+
+{"cod_articulo":"22430","nombre":"Help! Yellow Crewneck Help! Yellow Crewneck","descripcion":"Show your love for The Beatles with this crewneck sweatshirt!","precio":60.00,"descuento":20,"categoria":"Outfit","color":"Yellow","imagen":"Imagenes/E-Commerce/out2.jpg","cantidad":1,"subTotal":60.00,"size":0,"subCategoria":"Outlet","precDescuento":0}
